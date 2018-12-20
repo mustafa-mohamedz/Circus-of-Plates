f4a161c14105774056711b58034777b9a5020e66
OWNER <OWNER@DESKTOP-3CK9NLN>
2018-12-20 20:44:06 +0200

    singlton

--- src/eg/edu/alexu/csd/oop/game/object/Clown.java
+++ src/eg/edu/alexu/csd/oop/game/object/Clown.java
@@ -2,6 +2,7 @@
 
 package eg.edu.alexu.csd.oop.game.object;
 
+import eg.edu.alexu.csd.oop.game.object.movingStrategy.MovingStrategy;
 import eg.edu.alexu.csd.oop.game.world.InitialWorld;
 
 import java.awt.geom.AffineTransform;
@@ -14,26 +15,22 @@ import javax.imageio.ImageIO;
 
 public class Clown extends GameObjectImp {
     //public static BufferedImage img;
-
+	private static Clown singleClown;
+	private MovingStrategy movingStrategy= new MovingHorizontal();
     private Clown(BufferedImage[] clown) {
         super(124, 141, clown);
         setX(InitialWorld.img.getWidth()/2 - 124/2);
         setY(InitialWorld.img.getHeight() - 141);
         setVisible(true);
     }
-
-    @Override
-    public BufferedImage[] getSpriteImages() {
-        // TODO Auto-generated method stub
-
-        BufferedImage[] clown = new BufferedImage[1];
-        try {
-            img = ImageIO.read(new File("src/Images/clown.png"));
-            clown[0] = img;
-        } catch (IOException e) {
-            throw new RuntimeException("path not found");
-        }
-        return clown;
-    }
-
+	public static Clown GetClown() {
+		 BufferedImage[] clown = new BufferedImage[1];
+	        try {
+	            BufferedImage img = ImageIO.read(new File("src/Images/clown.png"));
+	            clown[0] = img;
+	        } catch (IOException e) {
+	            throw new RuntimeException("path not found");
+	        }
+	        singleClown = new Clown(clown);
+	}
 }
