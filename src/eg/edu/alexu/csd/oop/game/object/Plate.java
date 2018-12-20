4a062505965afe5e74e72c4e440ba5263acb7fa0
OWNER <OWNER@DESKTOP-3CK9NLN>
2018-12-20 22:39:15 +0200

    added Factory/flyWeight

--- src/eg/edu/alexu/csd/oop/game/object/Plate.java
+++ src/eg/edu/alexu/csd/oop/game/object/Plate.java
@@ -5,12 +5,15 @@ import java.awt.image.BufferedImage;
 
 public class Plate extends GameObjectImp {
 
-	private static String color;
+	private String color;
 	
 	public String getColor() {
 		return color;
 	}
-
+	
+	public void SetColor(String color) {
+		this.color = color;
+	}
 	public Plate(BufferedImage[] spriteImages, MovingStrategy movingStrategy) {
 		super(spriteImages, movingStrategy);
 		setVisible(true);
--- src/eg/edu/alexu/csd/oop/game/object/PlateFactory.java
+++ src/eg/edu/alexu/csd/oop/game/object/PlateFactory.java
@@ -1,12 +1,38 @@
 package eg.edu.alexu.csd.oop.game.object;
 
-import java.util.HashMap;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import eg.edu.alexu.csd.oop.game.object.movingStrategy.MovingVertical;
+import eg.edu.alexu.csd.oop.game.world.InitialWorld;
 
 public class PlateFactory {
-	
-	
+	private BufferedImage [][] plateImages;
+	private String [] colors;
 	public PlateFactory() {
-		// TODO Auto-generated constructor stub
+		try {
+			BufferedImage red = ImageIO.read(new File("src/Images/redplate.png"));
+			BufferedImage blue = ImageIO.read(new File("src/Images/blueplate.png"));
+			BufferedImage green = ImageIO.read(new File("src/Images/greenplate.png"));
+			plateImages = new BufferedImage[3][1];
+			plateImages[0][0] = red;
+			plateImages[1][0] = blue;
+			plateImages[2][0] = green;
+			colors = new String[]{"red","blue","green"};
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+	public Plate getRandomPlate() {
+		int colorId = (int) (Math.random()*3);
+		Plate RandomPlate = new Plate(plateImages[colorId],new MovingVertical((int) (Math.random()*InitialWorld.img.getWidth()), 10));
+		RandomPlate.SetColor(colors[colorId]);
+		return RandomPlate;
+	}
+	
 
 }
