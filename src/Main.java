3ce04883f128b5d1c2cca939aa05fdad4fe75b84
Ahmed Ashraf <engahmedashraf2210@gmail.com>
2018-12-23 02:22:01 +0200

    Sound is added

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ import java.awt.Color;
 import java.awt.image.BufferedImage;
 
 import javax.naming.InitialContext;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -26,9 +29,25 @@ public class Main {
 		menu.add(pauseMenuItem);
 		menu.add(resumeMenuItem);
 		menuBar.add(menu);
+		playSound("sound.wav");
 		
 		// TODO Auto-generated method stub
 		GameEngine.start("ayhabd", new InitialWorld(InitialWorld.img.getWidth(), InitialWorld.img.getHeight(), 100, new Observable()), menuBar, Color.BLACK);
 	}
+	public static synchronized void playSound(final String url) {
+		  new Thread(new Runnable() {
+		    public void run() {
+		      try {
+		        Clip clip = AudioSystem.getClip();
+		        AudioInputStream inputStream = AudioSystem.getAudioInputStream(
+		          Main.class.getResourceAsStream("Images//" + url));
+		        clip.open(inputStream);
+		        clip.start(); 
+		      } catch (Exception e) {
+		        System.err.println(e.getMessage());
+		      }
+		    }
+		  }).start();
+		}
 
 }
