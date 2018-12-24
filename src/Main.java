8c49e045c00fb3862af0fa4671920b0bf96b2e3f
AmrGeneidy <amr.geneidy.98@gmail.com>
2018-12-24 09:42:42 +0200

    adding main menu save/load back

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 
 import javax.naming.InitialContext;
 import javax.sound.sampled.AudioInputStream;
@@ -17,6 +18,7 @@ import eg.edu.alexu.csd.oop.game.GameEngine;
 import eg.edu.alexu.csd.oop.game.GameEngine.GameController;
 import eg.edu.alexu.csd.oop.game.Log;
 import eg.edu.alexu.csd.oop.game.MakeSounds;
+import eg.edu.alexu.csd.oop.game.Snapshot;
 import eg.edu.alexu.csd.oop.game.object.Observable;
 import eg.edu.alexu.csd.oop.game.world.InitialWorld;
 import eg.edu.alexu.csd.oop.game.world.levels.EasyLevel;
@@ -28,35 +30,61 @@ public class Main {
     public static void main(String[] args) {
 
         JMenuBar menuBar = new JMenuBar();
-        ;
         JMenu menu = new JMenu("Menu");
         JMenuItem newMenuItem = new JMenuItem("New Game");
         JMenuItem pauseMenuItem = new JMenuItem("Pause");
         JMenuItem resumeMenuItem = new JMenuItem("Resume");
+        final JMenuItem[] saveCheckPointMenuItem = {new JMenuItem("Save Check Point")};
+        JMenuItem loadCheckPointMenuItem = new JMenuItem("Load Check Point");
         menu.add(newMenuItem);
         menu.addSeparator();
         menu.add(pauseMenuItem);
         menu.add(resumeMenuItem);
+        menu.add(saveCheckPointMenuItem[0]);
+        menu.add(loadCheckPointMenuItem);
         menuBar.add(menu);
         MakeSounds.play("src/Images/sound.wav");
-
-        final GameController gameController = GameEngine.start("Circus Of Plates", new InitialWorld(InitialWorld.img.getWidth(), InitialWorld.img.getHeight(), new EasyLevel(), new Observable()), menuBar, Color.BLACK);
-		Log.getLoggeer().info("Start Game");
+        final Snapshot[] snapshot = {new Snapshot()};
+        final boolean[] snapShotIsActive = {false};
+        InitialWorld initialWorld = new InitialWorld(InitialWorld.img.getWidth(), InitialWorld.img.getHeight(), new EasyLevel(), new Observable());
+        final GameController[] gameController = {GameEngine.start("Circus Of Plates", initialWorld, menuBar, Color.BLACK)};
+        Log.getLoggeer().info("Start Game");
         pauseMenuItem.addActionListener(new ActionListener() {
-    		@Override public void actionPerformed(ActionEvent e) {
-    				gameController.pause();
-    				Log.getLoggeer().info("Pause Game");
-    			}
-    		});
-    		resumeMenuItem.addActionListener(new ActionListener() {
-    			@Override public void actionPerformed(ActionEvent e) {
-    				gameController.resume();
-    				Log.getLoggeer().info("Resume Game");
-    			}
-    		});
-        
-        
-        
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                gameController[0].pause();
+                Log.getLoggeer().info("Pause Game");
+            }
+        });
+        resumeMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                gameController[0].resume();
+                Log.getLoggeer().info("Resume Game");
+            }
+        });
+        saveCheckPointMenuItem[0].addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                snapshot[0] = initialWorld.getState();
+                snapShotIsActive[0] = true;
+                Log.getLoggeer().info("Check Point Saved");
+            }
+        });
+        loadCheckPointMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (snapShotIsActive[0]) {
+                    initialWorld.setState(snapshot[0]);
+                    Log.getLoggeer().info("Check Point Loaded");
+                    snapShotIsActive[0] = false;
+                } else {
+                    Log.getLoggeer().info("Couldn't Load Check Point");
+                }
+            }
+        });
+
+
     }
 
 }
