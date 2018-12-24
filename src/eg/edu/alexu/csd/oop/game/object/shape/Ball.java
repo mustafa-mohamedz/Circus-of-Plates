e4d3847c9f5c32cad61c6b6a42b2fb643feb2981
OWNER <OWNER@DESKTOP-3CK9NLN>
2018-12-24 02:44:04 +0200

    hope clone done

--- src/eg/edu/alexu/csd/oop/game/object/shape/Ball.java
+++ src/eg/edu/alexu/csd/oop/game/object/shape/Ball.java
@@ -3,6 +3,8 @@ package eg.edu.alexu.csd.oop.game.object.shape;
 import eg.edu.alexu.csd.oop.game.STATIC_VARS;
 import eg.edu.alexu.csd.oop.game.object.GameObjectImp;
 import eg.edu.alexu.csd.oop.game.object.shape.state.FreeFall;
+import eg.edu.alexu.csd.oop.game.object.shape.state.OnStickState;
+import eg.edu.alexu.csd.oop.game.object.shape.state.OutOfBoundries;
 import eg.edu.alexu.csd.oop.game.object.shape.state.ShapeState;
 import eg.edu.alexu.csd.oop.game.object.shape.state.State;
 
@@ -27,6 +29,18 @@ public class Ball extends Shape {
     public Object getName() {
         return color + "Ball";
     }
-	
+    @Override
+	public Shape clone() {
+		Ball x = new Ball(getSpriteImages(), getColor());
+		if (currentState.getState().equals(State.FREEFALL)) {
+			x.currentState = new FreeFall(getX(), getY());
+		}else if (currentState.getState().equals(State.ONLEFTSTICK)||currentState.getState().equals(State.ONRIGHTSTICK)) {
+			x.currentState = new OnStickState(currentState.getState(), movingStrategy, getWidth());
+		}else {
+			x.currentState = new OutOfBoundries(movingStrategy);
+		}
+		return x;
+	}
+
 
 }
--- src/eg/edu/alexu/csd/oop/game/object/shape/Plate.java
+++ src/eg/edu/alexu/csd/oop/game/object/shape/Plate.java
@@ -5,6 +5,8 @@ import java.awt.image.BufferedImage;
 import eg.edu.alexu.csd.oop.game.STATIC_VARS;
 import eg.edu.alexu.csd.oop.game.object.GameObjectImp;
 import eg.edu.alexu.csd.oop.game.object.shape.state.FreeFall;
+import eg.edu.alexu.csd.oop.game.object.shape.state.OnStickState;
+import eg.edu.alexu.csd.oop.game.object.shape.state.OutOfBoundries;
 import eg.edu.alexu.csd.oop.game.object.shape.state.ShapeState;
 import eg.edu.alexu.csd.oop.game.object.shape.state.State;
 
@@ -14,12 +16,7 @@ public class Plate extends Shape {
     public Plate(BufferedImage[] spriteImages, String color) {
         super(spriteImages, color);
     }
-
-    @Override
-    public GameObjectImp clone() {
-        return new Plate(getSpriteImages(), getColor());
-    }
-
+    
     @Override
     public int getWidth() {
         return STATIC_VARS.PLATE_WIDTH;
@@ -34,4 +31,17 @@ public class Plate extends Shape {
     public Object getName() {
         return color + "Plate";
     }
+
+	@Override
+	public Shape clone() {
+		Plate x = new Plate(getSpriteImages(), getColor());
+		if (currentState.getState().equals(State.FREEFALL)) {
+			x.currentState = new FreeFall(getX(), getY());
+		}else if (currentState.getState().equals(State.ONLEFTSTICK)||currentState.getState().equals(State.ONRIGHTSTICK)) {
+			x.currentState = new OnStickState(currentState.getState(), movingStrategy, getWidth());
+		}else {
+			x.currentState = new OutOfBoundries(movingStrategy);
+		}
+		return x;
+	}
 }
